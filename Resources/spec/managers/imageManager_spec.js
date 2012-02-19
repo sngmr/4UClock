987d64457cf7a2353f8698da613b88e672fedaaa
Shingo Mori <mori@connectionworks.jp>
2012-02-19 16:10:53 +0900

    Change file name upper and lower case (2)

---
+++ Resources/spec/managers/imageManager_spec.js
@@ -0,0 +1,58 @@
+describe("ImageManager", function() {
+	var manager, db, saveDir;
+	
+	beforeEach(function() {
+		manager = require('app/managers/imageManager');
+		db = require('app/common/dbutil').getDatabase();
+		saveDir = require('app/common/constant').IMAGE_FILE_DIR_NAME;
+	});
+	
+	it('Singleton test', function() {
+		var mgr1 = require('app/managers/imageManager');
+		var mgr2 = require('app/managers/imageManager');
+		
+		// Check the private value is not public
+		expect(mgr1._readPage).toBeUndefined();
+		expect(mgr2._readPage).toBeUndefined();
+		
+		// Check both manager should point same object
+		expect(mgr1).toBe(mgr2);
+	});
+	
+	it('Init test', function(){
+		var ready = false;
+		
+		Ti.App.addEventListener(manager.EVENT_READY, function() { ready = true; });
+		manager.init();
+		
+		waitsFor(function() { return ready; }, 'Never finish async method.', 30000);
+		
+		runs(function() {
+			var rs;
+			
+			rs = db.execute('SELECT * FROM feeds');
+			expect(rs.rowCount).toBeGreaterThan(10);
+			
+			rs = db.execute("SELECT * FROM feeds WHERE filename IS NOT NULL");
+			expect(rs.rowCount).toBeGreaterThan(0);
+		});
+	});
+	
+	it('Get image model test', function() {
+		var ready = false;
+		
+		Ti.App.addEventListener(manager.EVENT_READY, function() { ready = true; });
+		manager.init();
+		
+		waitsFor(function() { return ready; }, 'Never finish async method.', 30000);
+		
+		runs(function() {
+			// At least 5 data can get
+			expect(Titanium.Filesystem.getFile(saveDir, manager.getNext().filename).exists()).toBeTruthy();
+			expect(Titanium.Filesystem.getFile(saveDir, manager.getNext().filename).exists()).toBeTruthy();
+			expect(Titanium.Filesystem.getFile(saveDir, manager.getNext().filename).exists()).toBeTruthy();
+			expect(Titanium.Filesystem.getFile(saveDir, manager.getNext().filename).exists()).toBeTruthy();
+			expect(Titanium.Filesystem.getFile(saveDir, manager.getNext().filename).exists()).toBeTruthy();
+		});
+	});
+});
--- Resources/spec/models/Feed_spec.js
+++ Resources/spec/models/Feed_spec.js
@@ -8,11 +8,13 @@ describe('Feed', function() {
 		db.execute('DELETE FROM feeds');
 	});
 	
-	it('Select all test', function() {
+	it('Private value visibility test', function() {
 		var feed = new (require('app/models/Feed'))();
-		
-		// Check if private instance value can see
 		expect(feed._db).toBeUndefined();
+	});
+	
+	it('selectAll', function() {
+		var feed = new (require('app/models/Feed'))();
 		
 		db.execute("INSERT INTO feeds VALUES (null,'title1','image1','20120101','filepath1',0, 0)");
 		db.execute("INSERT INTO feeds VALUES (null,'title2','image2','20120102','filepath2',0, 0)");
@@ -25,7 +27,7 @@ describe('Feed', function() {
 		expect(rows[0].pubdate).toEqual('20120101');
 	});
 	
-	it('Select display test 1', function() {
+	it('selectDisplay with no arguments', function() {
 		var feed = new (require('app/models/Feed'))();
 		
 		db.execute("INSERT INTO feeds VALUES (null,'title1','image1','20120101','filepath1',0, 0)");
@@ -35,26 +37,26 @@ describe('Feed', function() {
 		var rows = feed.selectDisplay();
 		
 		expect(rows.length).toEqual(2);
-		expect(rows[0].title).toEqual('title3');
-		expect(rows[1].image_url).toEqual('image1');
-		expect(rows[0].pubdate).toEqual('20120103');
-	});
-	
-	it('Select display test 2', function() {
-		var feed = new (require('app/models/Feed'))();
-		
-		db.execute("INSERT INTO feeds VALUES (null,'title1','image1','20120101','filepath1',0, 0)");
-		db.execute("INSERT INTO feeds VALUES (null,'title2','image2','20120102',NULL,0, 0)");
-		db.execute("INSERT INTO feeds VALUES (null,'title3','image3','20120103','filepath3',0, 0)");
-		
-		var rows = feed.selectDisplay('20120102');
-		
-		expect(rows.length).toEqual(1);
 		expect(rows[0].title).toEqual('title1');
+		expect(rows[1].image_url).toEqual('image3');
 		expect(rows[0].pubdate).toEqual('20120101');
 	});
 	
-	it('Select only undownload', function() {
+	it('selectDisplay with arguments', function() {
+		var feed = new (require('app/models/Feed'))();
+		
+		db.execute("INSERT INTO feeds VALUES (1,'title1','image1','20120101','filepath1',0, 0)");
+		db.execute("INSERT INTO feeds VALUES (2,'title2','image2','20120102',NULL,0, 0)");
+		db.execute("INSERT INTO feeds VALUES (3,'title3','image3','20120103','filepath3',0, 0)");
+		
+		var rows = feed.selectDisplay(2);
+		
+		expect(rows.length).toEqual(1);
+		expect(rows[0].title).toEqual('title3');
+		expect(rows[0].pubdate).toEqual('20120103');
+	});
+	
+	it('selectUndownload', function() {
 		var feed = new (require('app/models/Feed'))();
 		
 		db.execute("INSERT INTO feeds VALUES (null,'title1','image1','20120101',NULL,0, 0)");
@@ -67,18 +69,7 @@ describe('Feed', function() {
 		expect(rows[0].filename).toEqual(null);
 	});
 	
-	it('Insert test 1', function() {
-		var feed = new (require('app/models/Feed'))();
-		
-		var count = feed.insert([null, 'title1', 'image1', 'pubdate1', 'filepath1', 0, 0]);
-		expect(count).toEqual(1);
-		
-		var rows = feed.selectAll();
-		expect(rows.length).toEqual(1);
-		expect(rows[0].title).toEqual('title1');
-	});
-	
-	it('Insert test 2', function() {
+	it('insert', function() {
 		var feed = new (require('app/models/Feed'))();
 		
 		var count = feed.insert([null, 'title1', 'image1', 'pubdate1', 'filepath1', 0, 0]);
@@ -92,7 +83,7 @@ describe('Feed', function() {
 		expect(rows[1].title).toEqual('title2');
 	});
 	
-	it('Update file test 1', function() {
+	it('updateFileData', function() {
 		var feed = new (require('app/models/Feed'))();
 		
 		db.execute("INSERT INTO feeds VALUES (null,'title1','image1','20120101',NULL,0,0)");
@@ -108,6 +99,21 @@ describe('Feed', function() {
 		expect(rows[0].filename).toEqual('hogehoge');
 		expect(rows[0].width).toEqual(100);
 		expect(rows[0].height).toEqual(234);
+	});
+	
+	it('remove', function() {
+		var feed = new (require('app/models/Feed'))();
+		
+		db.execute("INSERT INTO feeds VALUES (1,'title1','image1','20120101',NULL,0,0)");
+		
+		var rows = feed.selectAll();
+		expect(rows.length).toEqual(1);
+		
+		var count = feed.remove(1);
+		expect(count).toEqual(1);
+		
+		var rows = feed.selectAll();
+		expect(rows.length).toEqual(0);
 	});
 	
 });
