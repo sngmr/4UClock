4359332cefd55b3cc8a8e95247879544ed1433b5
Shingo Mori <mori@connectionworks.jp>
2012-03-19 19:23:26 +0900

    Arrange view's function and add splash screen for iPad

--- Resources/app/views/ClockWindow.js
+++ Resources/app/views/ClockWindow.js
@@ -1,18 +1,102 @@
 /**
  * Clock Window
- * It's way to implement...
  */
-var _self, _imageViewContainer, _imageViews, _clockLabel, _emergencyView;
+// UI components
+var _self, _imageViewContainer, _imageViews, _clockLabel, _emergencyView, _emergencyClockLabel;
+
+var _common;
 var _imageManager;
-var _switchCounter;
-var _showEmergency;
+var _imageSwitchCounter;
+var _isEmergencyMode;
 
 function ClockWindow(imageManager) {
+	_common = require('/app/common/common');
 	_imageManager = imageManager;
-	_switchCounter = 0;
-	_showEmergency = false;
+	_imageSwitchCounter = 0;
+	_isEmergencyMode = false;
 	
-	// Setting UI components
+	// Build view
+	_buildView();
+	
+	// Add event listener to view components
+	_self.addEventListener('click', _toggleEmergencyMode);
+	Ti.Gesture.addEventListener('orientationchange', _adjustEmergencyModeWhenRotate);
+	
+	// Start timer for clock (+1sec is adjustment first launch)
+	setTimeout(_startClock, 60000 - ((new Date()).getSeconds() * 1000) + 1000);
+	
+	// Set initial image and clock
+	_change();
+	
+	return _self;
+}
+
+function _startClock() {
+	_change();
+	setInterval(_change, 60000);
+}
+
+function _change() {
+	_changeClock();
+	_changeImage();
+}
+
+function _changeImage() {
+	// Detect which image view is currently showing
+	var currentImageView = _imageViews[_imageSwitchCounter % 2];
+	_imageSwitchCounter++;
+	var nextImageView = _imageViews[_imageSwitchCounter % 2];
+	
+	// Change next image view's image with fade animation
+	var imageFileData = _imageManager.getNext();
+	if (!imageFileData) {
+		Ti.API.error('[ClockWindow]Woops!! ImageManager dose NOT have next image!!! I am waiting...');
+		return;
+	}
+	
+	// Set image
+	nextImageView.setImage(
+		Ti.Filesystem.getFile(_imageManager.IMAGE_FILE_DIR_NAME, imageFileData.image_file_name).nativePath);
+	
+	// Change image
+	currentImageView.animate({ opacity: 0, duration: 750 });
+	nextImageView.animate({ opacity: 1, duration: 750 });
+}
+
+function _changeClock() {
+	var date = new Date();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	_clockLabel.setText(_common.twoZeroPadding(hour) + ':' + _common.twoZeroPadding(minute));
+	_emergencyClockLabel.setText(_common.twoZeroPadding(hour) + ':' + _common.twoZeroPadding(minute));
+}
+
+function _toggleEmergencyMode(event) {
+	// Stop animation
+	_emergencyView.animate();
+	
+	if (_isEmergencyMode) {
+		// 19 is for height of "warning.png". Want to hide warning.png after animation
+		_emergencyView.animate(
+			{ top: 0 - Ti.Platform.displayCaps.getPlatformHeight() - 19, duration: 1000 }
+		);
+		Titanium.App.idleTimerDisabled = true;
+	} else {
+		_emergencyView.animate({ top: 0, duration: 250 });
+		Titanium.App.idleTimerDisabled = false;
+	}
+	_isEmergencyMode = !_isEmergencyMode;
+}
+
+function _adjustEmergencyModeWhenRotate(event) {
+	_emergencyView.height = Ti.Platform.displayCaps.getPlatformHeight() + 19;
+	if (!_isEmergencyMode) {
+		_emergencyView.top = 0 - Ti.Platform.displayCaps.getPlatformHeight() - 19;
+	}
+}
+
+function _buildView() {
+	// Window
 	_self = Ti.UI.createWindow({
 		fullscreen: true,
 		orientationModes: [Ti.UI.PORTRAIT, Ti.UI.UPSIDE_PORTRAIT, Ti.UI.LANDSCAPE_LEFT, Ti.UI.LANDSCAPE_RIGHT],
@@ -49,8 +133,8 @@ function ClockWindow(imageManager) {
 	});
 	_clockLabel = Ti.UI.createLabel({
 		text: '',
-		width: '100%',
 		textAlign: 'center',
+		width: '100%',
 		height: 'auto',
 		color: '#FFFFFF',
 		font: { fontWeight: 'bold', fontSize: 32 },
@@ -60,15 +144,23 @@ function ClockWindow(imageManager) {
 	_imageViewContainer.add(header);
 	
 	_self.add(_imageViewContainer);
-	_imageViewContainer.addEventListener('click', _toggleEmergencyMode);
 	
 	// Emergency view
 	_emergencyView = Ti.UI.createView({
-		top: -1,
+		top: 0 - Ti.Platform.displayCaps.getPlatformHeight() - 19,
 		left: 0,
 		width: '100%',
-		height: 1,
+		height: Ti.Platform.displayCaps.getPlatformHeight() + 19,
 		backgroundColor: '#000000',
+	});
+	_emergencyClockLabel = Ti.UI.createLabel({
+		text: '',
+		textAlign: 'center',
+		width: '100%',
+		height: 'auto',
+		color: '#FFFFFF',
+		font: { fontWeight: 'bold', fontSize: 72 },
+		opacity: 0.35,
 	});
 	var warningImage = Ti.UI.createImageView({
 		image: '/images/warning.png',
@@ -76,64 +168,10 @@ function ClockWindow(imageManager) {
 		width: 1024,
 		height: 19,
 	});
+	_emergencyView.add(_emergencyClockLabel);
 	_emergencyView.add(warningImage);
+	
 	_self.add(_emergencyView);
-	_emergencyView.addEventListener('click', _toggleEmergencyMode);
-	
-	// Start timer for change clock and image
-	_changeDisplay();
-	
-	// TODO Uhhhhhmmm. It's not good for the clock. Need to change.
-	var date = new Date();
-	setTimeout(_startClockTimer, 10000);
-	// setTimeout(_startClockTimer, 60000 - (date.getSeconds() * 1000 + date.getMilliseconds()));
-	
-	return _self;
-}
-
-function _startClockTimer() {
-	_changeDisplay();
-	setInterval(_changeDisplay, 10000);
-}
-
-function _changeDisplay() {
-	// Set time
-	_clockLabel.setText(_getTime());
-	
-	// Detect which image view is currently showing
-	var currentImageView = _imageViews[_switchCounter % 2];
-	_switchCounter++;
-	var nextImageView = _imageViews[_switchCounter % 2];
-	
-	// Change next image view's image with fade animation
-	var imageFileData = _imageManager.getNext();
-	if (!imageFileData) {
-		Ti.API.error('[ClockWindow]Woops!! ImageManager dose NOT have next image!!! I am waiting...');
-		return;
-	}
-	
-	nextImageView.setImage(Ti.Filesystem.getFile(_imageManager.IMAGE_FILE_DIR_NAME, imageFileData.image_file_name).nativePath);
-	currentImageView.animate({ opacity: 0, duration: 750 });
-	nextImageView.animate({ opacity: 1, duration: 750 });
-}
-
-function _getTime() {
-	var common = require('/app/common/common');
-	var date = new Date();
-	var hour = date.getHours();
-	var minute = date.getMinutes();
-	
-	return common.twoZeroPadding(hour) + ':' + common.twoZeroPadding(minute);
-}
-
-function _toggleEmergencyMode(event) {
-	if (_showEmergency) {
-		_emergencyView.animate({ height: 1, duration: 1000 }, function() { _emergencyView.height = 1; });
-	} else {
-		// 20 is for "top: -1" AND "warning.png"
-		_emergencyView.animate({ height: Ti.Platform.displayCaps.platformHeight + 20, duration: 250 }, function() { _emergencyView.height = Ti.Platform.displayCaps.platformHeight + 20; });
-	}
-	_showEmergency = !_showEmergency;
 }
 
 // Export
